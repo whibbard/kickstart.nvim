41762368e61d8ffc01771727804e972dd86a4cc6
Will Hibbard <whibbard@squareup.com>
2024-06-04 11:17:41 -0400

    add copilot

---
+++ lua/custom/plugins/github-copilot.lua
@@ -0,0 +1,14 @@
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  build = ':Copilot auth',
+  opts = {
+    suggestion = { enabled = true },
+    panel = { enabled = true },
+    filetypes = {
+      markdown = true,
+      help = true,
+      typescript = true,
+    },
+  },
+}
